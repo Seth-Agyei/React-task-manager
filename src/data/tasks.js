4920789e282676631708a139e7c15da4d08b9c05
Seth-Agyei <seth199299@gmail.com>
2026-06-16 18:32:37 +0100

    Dataset tasklist

---
+++ src/data/tasks.js
@@ -0,0 +1,24 @@
+
+const tasks = [
+
+    {
+        id: 1,
+        title: "Build Nav",
+        status: "Completed"
+    },
+    {
+        id: 2,
+        title: "Create Dashboard",
+        status: "In progress"
+    },
+    {
+        id: 3,
+        title: "Implement Router",
+        status: "Pending"
+    }
+    
+    
+
+]
+
+export default tasks;
